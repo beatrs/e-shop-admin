2fa1d27e956b1db326684e77181890678c3d6780
beatrs <201811991@feualabang.edu.ph>
2022-09-12 15:27:41 +0800

    change api url

--- src/reqMethods.js
+++ src/reqMethods.js
@@ -2,7 +2,8 @@ import axios from "axios"
 
 
 const LOCAL_URL = "http://localhost:5000/api/"
-const PROD_URL = "https://wiz-shop.herokuapp.com/api/"
+// const PROD_URL = "https://wiz-shop.herokuapp.com/api/"
+const PROD_URL = "https://e-shop-api.onrender.com/"
 
 const API_URL = process.env.NODE_ENV === 'production' ? PROD_URL : LOCAL_URL
 
